39eb67482a8bab446efb652c0b0d6007e92d4c9f
OMarceloF <the.marcelof@gmail.com>
2024-08-09 08:48:44 -0300

    Funcionalidade de pesquisa e de alfabeto crescente nas unidades

--- client/src/Components/Dashboard/Unidades/Unidades.jsx
+++ client/src/Components/Dashboard/Unidades/Unidades.jsx
@@ -7,24 +7,41 @@ const Unidades = () => {
     const location = useLocation();
 
     const [unidades, setUnidades] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
     useEffect(() => {
         axios.get('http://localhost:3002/unidades')
             .then(response => {
-                setUnidades(response.data);
+                // Ordena as unidades em ordem alfabética com base no nome da cidade
+                const sortedUnidades = response.data.sort((a, b) => a.cidade.localeCompare(b.cidade));
+                setUnidades(sortedUnidades);
             })
             .catch(error => {
                 console.error('Erro ao buscar dados:', error);
             });
     }, []);
 
+    // Filtra as unidades conforme o termo de pesquisa
+    const filteredUnidades = unidades.filter(unidade =>
+        unidade.cidade.toLowerCase().startsWith(searchTerm.toLowerCase())
+    );
+
     return (
         <>
             {location.pathname === "/dashboard/unidades" ? (
                 <>
                     <h2>Unidades</h2>
+                    <div className="search-container">
+                        <input
+                            type="text"
+                            placeholder="Buscar unidade..."
+                            value={searchTerm}
+                            onChange={(e) => setSearchTerm(e.target.value)}
+                        />
+                        <span className="search-icon">🔍</span>
+                    </div>
                     <div className="list-container">
-                        {unidades.map((unidade, index) => (
+                        {filteredUnidades.map((unidade, index) => (
                             <div key={index} className="list-item">
                                 <span>{unidade.cidade}</span>
                                 <Link to={`/dashboard/unidades/editar/${unidade.id}`}>
