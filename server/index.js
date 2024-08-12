2f1943fde53c195ae7cca8bdd43bfdfe846b3d26
OMarceloF <the.marcelof@gmail.com>
2024-08-12 09:26:46 -0300

    Responsividade

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
 
 app.use(express.json());
 app.use(cors());
